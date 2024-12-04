a26ff2f744215040262ceb8c996b447acb8c917e
jamesgober <james.gober@icloud.com>
2024-12-03 21:31:14 -0500

    Updated Tests

---
+++ tests/Parsers/YmlParserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JG\Tests\Parsers;
+
+use PHPUnit\Framework\TestCase;
+use JG\Config\Parsers\YamlParser;
+use JG\Config\Exceptions\ConfigParseException;
+
+class YmlParserTest extends TestCase
+{
+    public function testYamlParser()
+    {
+        $parser = new YamlParser();
+        $filePath = __DIR__ . '/../config/config.yml';
+
+        $config = $parser->parse($filePath);
+
+        $this->assertEquals('localhost', $config['database']['host']);
+        $this->assertEquals(3306, $config['database']['port']);
+        $this->assertTrue($config['app']['debug']);
+        $this->assertNull($config['app']['cache']);
+    }
+
+    public function testYamlParserInvalidFile()
+    {
+        $this->expectException(ConfigParseException::class);
+
+        $parser = new YamlParser();
+        $parser->parse(__DIR__ . '/../config/nonexistent.yml');
+    }
+}
